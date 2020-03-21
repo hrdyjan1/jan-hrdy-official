fd6cf9bb54be134a3a351d8844ef928772e8d7fd
Hrdas <hrdyjan1@gmail.com>
2020-03-21 10:19:18 +0100

    Menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
-const inputs = document.querySelectorAll(".input");
+// elements
+var $page = $('.page');
 
-
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+$('.menu_toggle').on('click', function(){
+  $page.toggleClass('shazam');
 });
+$('.content').on('click', function(){
+  $page.removeClass('shazam');
+});
+
+
+$('#moar').on('click', () => console.log('Moar'))
