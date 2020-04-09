d9ea96a126550ee6c54c61db9bf4e99e4d495d0c
Hrdas <hrdyjan1@gmail.com>
2020-04-09 13:32:31 +0200

    Finilized toggle

--- pages/_document.js
+++ pages/_document.js
@@ -15,18 +15,15 @@ class MyDocument extends Document {
             rel='stylesheet'
             href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'
           />
-          <link
-            href='https://fonts.googleapis.com/css?family=Crimson+Pro:300,400,600,700&display=swap'
-            rel='stylesheet'
-          />
-          <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.4.1/css/all.css' /> 
+          {/* <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.4.1/css/all.css' />  */}
           {/* Jou tohle je na ikony socialni */}
 
-          <link rel='stylesheet' type='text/css' href='/css/welcome/font-awesome.min.css' />
+          {/* <link rel='stylesheet' type='text/css' href='/css/welcome/font-awesome.min.css' /> */}
           <link rel='stylesheet' type='text/css' href='/css/welcome/toggle.css' />
           <link rel='stylesheet' type='text/css' href='/css/welcome/icon.css' />
           <link rel='stylesheet' type='text/css' href='/css/welcome/form.css' />
           <link rel='stylesheet' type='text/css' href='/css/welcome/index.css' />
+
           {/* <link rel='stylesheet' type='text/css' href='/css/loading.css' /> */}
           {/* <link rel='stylesheet' type='text/css' href='/css/nprogress.css' /> */}
           <link rel='stylesheet' type='text/css' href='/css/toggle.css' />
