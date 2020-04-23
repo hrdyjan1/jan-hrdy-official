5a8674b893c1aec3a0a542cee039f45142593339
Hrdas <hrdyjan1@gmail.com>
2020-04-23 19:35:28 +0200

    Seted up clickable clocks

---
+++ components/pages/projects/Clock.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const rotate = (hr, mn, sc) => (finalHour, finalMinute, finalSecond) => {
+  const deg = 6;
+  let day = new Date();
+
+  let hh = (finalHour ?? day.getHours()) * 30;
+  let mm = (finalMinute ?? day.getMinutes()) * deg;
+  let ss = (finalSecond ?? day.getSeconds()) * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+};
+
+const Clock = ({ staticDate, shouldStartOnClickClock }) => {
+  const [isClickedOnClock, setClickedOnClock] = React.useState(false);
+
+  const setClockReady = () => setClickedOnClock(true);
+
+  React.useEffect(() => {
+    let interval;
+    const hr = document.getElementById('hr');
+    const mn = document.getElementById('mn');
+    const sc = document.getElementById('sc');
+    const rotation = rotate(hr, mn, sc);
+    const shouldRunClock = isClickedOnClock && shouldStartOnClickClock;
+    console.log('isClickedOnClock', isClickedOnClock)
+    console.log('shouldStartOnClickClock', shouldStartOnClickClock)
+
+    if (staticDate && !shouldRunClock) {
+      rotation(staticDate?.hr, staticDate?.mn, staticDate?.sc);
+    } else {
+      interval = setInterval(rotation);
+    }
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [staticDate, shouldStartOnClickClock, isClickedOnClock]);
+
+  return (
+    <div className='clock' onClick={setClockReady}>
+      <div className='hour'>
+        <div className='hr' id='hr'></div>
+      </div>
+      <div className='min'>
+        <div className='mn' id='mn'></div>
+      </div>
+      <div className='sec'>
+        <div className='sc' id='sc'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Clock;
--- pages/404.js
+++ pages/404.js
@@ -1,47 +1,11 @@
 import React from 'react';
 import Intro from '../components/pages/projects/Intro';
-
-const Clock = () => {
-  React.useEffect(() => {
-    const deg = 6;
-    const hr = document.getElementById('hr');
-    const mn = document.getElementById('mn');
-    const sc = document.getElementById('sc');
-
-    const interval = setInterval(() => {
-      let day = new Date();
-      let hh = day.getHours() * 30;
-      let mm = day.getMinutes() * deg;
-      let ss = day.getSeconds() * deg;
-
-      hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-      mn.style.transform = `rotateZ(${mm}deg)`;
-      sc.style.transform = `rotateZ(${ss}deg)`;
-    });
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className='clock'>
-      <div className='hour'>
-        <div className='hr' id='hr'></div>
-      </div>
-      <div className='min'>
-        <div className='mn' id='mn'></div>
-      </div>
-      <div className='sec'>
-        <div className='sc' id='sc'></div>
-      </div>
-    </div>
-  );
-};
+import Clock from '../components/pages/projects/Clock';
 
 const Page404 = () => {
   return (
     <div id='page-404-id'>
-      <Clock />
+      <Clock staticDate={{ hr: 4, mn: 4, sc: 0 }} shouldStartOnClickClock/>
       <Intro header='404' subHeader='description404' />
     </div>
   );
