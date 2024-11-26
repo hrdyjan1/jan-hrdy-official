41efcab85fd0e8ed1ed27d60752e6a9118673f65
Jan Hrdý <jan.hrdy@carvago.com>
2024-11-26 08:33:55 +0100

    Zkouska CV

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,12 +1,36 @@
 // Add this at the top of your file to mark it as a client component
 'use client';
 
+import { useWindowWidth } from '@/hooks/useWindowWidth';
 import React from 'react';
 
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url
+// ).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
+
+const file: React.ComponentProps<typeof Document>['file'] = { url: './cv.pdf' };
+
 function CV() {
+  const width = useWindowWidth();
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <p>CV - TODO</p>
+      <Document
+        file={file}
+        onLoadError={(e) => console.log('Error while loading document! ' + e.message)}
+        onSourceError={(e) => console.log('Error while loading document! ' + e.message)}
+      >
+        <Page pageNumber={1} width={Math.min(width, 1200)} />
+        <Page pageNumber={2} width={Math.min(width, 1200)} />
+        <br />
+      </Document>
     </div>
   );
 }
